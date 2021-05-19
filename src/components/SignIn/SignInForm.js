c4ae0603ac9ba53b9ada7af51b549f68997a3d3d
Poro <poro@poros-macbook-pro.home>
2021-05-19 18:02:58 +0200

    styling create event

--- src/components/SignIn/SignInForm.js
+++ src/components/SignIn/SignInForm.js
@@ -1,12 +1,23 @@
 import classes from './SignInForm.module.css'
+import axios from 'axios'
+import { useState } from 'react'
 
 function SignInForm() {
+    const [mail, setMail] = useState("")
+    const handleSignIn = (e) => {
+        e.preventDefault()
+        axios.get("https://api.signally.io/auth/magiclink/send?email=" + mail).then((res) => {
+            console.log(res)
+        })
+    }
     return (
         <div className={classes.container}>
             <p>Veuillez entrer votre email pour accéder à votre signature.
             Vous pourrez ainsi la mettre à jour quand vous voulez.</p>
-            <input type="mail" />
-            <button>Envoyez moi un Magic Link</button>
+            <form onSubmit={(e) => handleSignIn(e)}>
+                <input type="mail" value={mail} onChange={(mail) => setMail(mail.target.value)} />
+                <button>Envoyez moi un Magic Link</button>
+            </form>
         </div>
     )
 }
