be593db5cce67068fdc8654aefb1c49ea504f490
fciprian <fciprian@student.42.fr>
2022-03-09 10:29:54 +0100

    bold company & adress

--- src/Desktop/views/Signatures/create/TemplateSelect/templateSelect.js
+++ src/Desktop/views/Signatures/create/TemplateSelect/templateSelect.js
@@ -1,16 +1,17 @@
-import classes from './templateSelect.module.css'
-import { useState } from 'react';
+import classes from "./templateSelect.module.css";
+import { useState } from "react";
 // import Template from './template';
-import Button from 'Utils/Button/btn';
-import Template from '../Preview/customizablePreview';
+import Button from "Utils/Button/btn";
+import Template from "../Preview/customizablePreview";
 
 // Displaying the list of bought and free templates (Studio, Store) and allows to select one to create custom signature
 
-const templateAPI = [{
-	id: 1,
-	alignment: "Horizontal",
-	tags: ["studio"],
-	html: `
+const templateAPI = [
+    {
+        id: 1,
+        alignment: "Horizontal",
+        tags: ["studio"],
+        html: `
 	{# START GREETINGS #}
 	<span style="padding-bottom: {{ styles['greetingsPadding']['padding'] }};">
 	{% if greetings %}
@@ -59,13 +60,13 @@ const templateAPI = [{
 														<tr height="5"></tr>
 														<tr>
 															<td>
-																<span style="color:{{ styles['companyName']['color'] }}; text-decoration: {{ styles['companyName']['textDecoration'] }} ;font-style: {{ styles['companyName']['fontStyle'] }}font-weight: {{ styles['companyName']['fontWeight'] }}font-family: {{ styles['generalFontFamily']['fontFamily'] }}; font-size: {{ styles['generalFontSize']['fontSize'] }};">
+																<span style="color:{{ styles['companyName']['color'] }}; text-decoration: {{ styles['companyName']['textDecoration'] }} ;font-style: {{ styles['companyName']['fontStyle'] }}; font-weight: {{ styles['companyName']['fontWeight'] }}; font-family: {{ styles['generalFontFamily']['fontFamily'] }}; font-size: {{ styles['generalFontSize']['fontSize'] }};">
                                                                 {{ company.name }}</span>
 															</td>
 														</tr>
 														<tr>
 															<td>
-																<span style="color:{{ styles['addressStreet']['color'] }}; text-decoration: {{ styles['addressStreet']['textDecoration'] }} ;font-style: {{ styles['addressStreet']['fontStyle'] }}font-weight: {{ styles['addressStreet']['fontWeight'] }}font-family: {{ styles['generalFontFamily']['fontFamily'] }}; font-size: {{ styles['generalFontSize']['fontSize'] }};">{{ address.street }}
+																<span style="color:{{ styles['addressStreet']['color'] }}; text-decoration: {{ styles['addressStreet']['textDecoration'] }} ;font-style: {{ styles['addressStreet']['fontStyle'] }}; font-weight: {{ styles['addressStreet']['fontWeight'] }}; font-family: {{ styles['generalFontFamily']['fontFamily'] }}; font-size: {{ styles['generalFontSize']['fontSize'] }};">{{ address.street }}
 																	{{ address.streetInfo }}
 																</span>
 															</td>
@@ -189,83 +190,159 @@ const templateAPI = [{
 	{# END DISCLAIMERS #}
 </span>
 
-`
-}]
+`,
+    },
+];
 
 export default function TemplateSelection(props) {
-	const [orientation, setOrientation] = useState("Horizontal")
-	const [tag, setTag] = useState(true)
-	const handleForm = (e) => {
-		props.setTemplate(JSON.parse(e.target.value))
-	}
+    const [orientation, setOrientation] = useState("Horizontal");
+    const [tag, setTag] = useState(true);
+    const handleForm = (e) => {
+        props.setTemplate(JSON.parse(e.target.value));
+    };
 
-	const handleAlignment = (e) => {
-		setOrientation(e.target.id)
-	}
-	const handleStudio = (e) => {
-		setTag(e.target.checked)
-	}
-	return (<div className={classes.modal}>
-		<div className={classes.searchContainer}>
-			<h1>Choisissez votre modèle de signature</h1>
-			<div className={classes.tagsContainer}>
-				<form onChange={handleStudio}>
-					<ul className={classes.studioStore}>
-						<li className={`${classes.studio} ${tag ? classes.activeStudio : ""}`}>
-							<input type="checkbox" defaultChecked={tag} id="studio" />My Studio
-						</li>
-						<li className={classes.store}>My Store</li>
-					</ul>
-				</form>
-				<div className={classes.otherTagsContainer}>
-					<ul className={classes.otherTags}>
-						<li>Classique</li>
-						<li>Élegant</li>
-						<li>Créatif</li>
-					</ul>
-				</div>
-			</div>
-			<div className={classes.orientationContainer}>
-				<form onChange={handleAlignment}>
-					<label className={classes.radioCtr} htmlFor="horizontal">Horizontal
-						<input type="radio" defaultChecked={true} id="horizontal" name="orientation" />
-						<span className={classes.checkmark}></span>
-					</label>
-					<label className={classes.radioCtr} htmlFor="panoramique">Panoramique
-						<input type="radio" id="panoramique" name="orientation" />
-						<span className={classes.checkmark}></span>
-					</label>
-					<label className={classes.radioCtr} htmlFor="vertical">Vertical
-						<input type="radio" id="vertical" name="orientation" />
-						<span className={classes.checkmark}></span>
-					</label>
-				</form>
-			</div>
-		</div>
-		<form onChange={handleForm}>
-			<ul className={classes.templatesContainer}>
-				{templateAPI.map((template) => {
-					if (template.alignment.toLowerCase() === orientation.toLowerCase())
-						if (tag) {
-							if (template.tags[0].toLowerCase() === "studio") {
-								return (<li key={template.id}>
-									<p className={classes.templateName}>Mama Shelter</p>
-									<input readOnly type="radio" name="template" value={JSON.stringify(template)} />
-									<Template options={{ event: { enabled: true } }} template={template.html} socials={props.icons} />
-								</li>)
-							}
-						}
-						else
-							return (<li key={template.id}>
-								<input type="radio" name="template" value={template.html} />
-								<Template template={template.html} socials={props.icons} organisation={props.organisation} />
-							</li>)
-				})}
-				{!tag ? <li style={{ width: "412px", height: "220px" }}></li> : ""}
-			</ul>
-			<div className={classes.btnContainer}>
-				<Button color="orange" width={'5rem'} onClick={(e) => { e.preventDefault(); props.showFunction() }}>Annuler</Button>
-			</div>
-		</form>
-	</div>)
+    const handleAlignment = (e) => {
+        setOrientation(e.target.id);
+    };
+    const handleStudio = (e) => {
+        setTag(e.target.checked);
+    };
+    return (
+        <div className={classes.modal}>
+            <div className={classes.searchContainer}>
+                <h1>Choisissez votre modèle de signature</h1>
+                <div className={classes.tagsContainer}>
+                    <form onChange={handleStudio}>
+                        <ul className={classes.studioStore}>
+                            <li
+                                className={`${classes.studio} ${
+                                    tag ? classes.activeStudio : ""
+                                }`}
+                            >
+                                <input
+                                    type="checkbox"
+                                    defaultChecked={tag}
+                                    id="studio"
+                                />
+                                My Studio
+                            </li>
+                            <li className={classes.store}>My Store</li>
+                        </ul>
+                    </form>
+                    <div className={classes.otherTagsContainer}>
+                        <ul className={classes.otherTags}>
+                            <li>Classique</li>
+                            <li>Élegant</li>
+                            <li>Créatif</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={classes.orientationContainer}>
+                    <form onChange={handleAlignment}>
+                        <label
+                            className={classes.radioCtr}
+                            htmlFor="horizontal"
+                        >
+                            Horizontal
+                            <input
+                                type="radio"
+                                defaultChecked={true}
+                                id="horizontal"
+                                name="orientation"
+                            />
+                            <span className={classes.checkmark}></span>
+                        </label>
+                        <label
+                            className={classes.radioCtr}
+                            htmlFor="panoramique"
+                        >
+                            Panoramique
+                            <input
+                                type="radio"
+                                id="panoramique"
+                                name="orientation"
+                            />
+                            <span className={classes.checkmark}></span>
+                        </label>
+                        <label className={classes.radioCtr} htmlFor="vertical">
+                            Vertical
+                            <input
+                                type="radio"
+                                id="vertical"
+                                name="orientation"
+                            />
+                            <span className={classes.checkmark}></span>
+                        </label>
+                    </form>
+                </div>
+            </div>
+            <form onChange={handleForm}>
+                <ul className={classes.templatesContainer}>
+                    {templateAPI.map((template) => {
+                        if (
+                            template.alignment.toLowerCase() ===
+                            orientation.toLowerCase()
+                        )
+                            if (tag) {
+                                if (
+                                    template.tags[0].toLowerCase() === "studio"
+                                ) {
+                                    return (
+                                        <li key={template.id}>
+                                            <p className={classes.templateName}>
+                                                Mama Shelter
+                                            </p>
+                                            <input
+                                                readOnly
+                                                type="radio"
+                                                name="template"
+                                                value={JSON.stringify(template)}
+                                            />
+                                            <Template
+                                                options={{
+                                                    event: { enabled: true },
+                                                }}
+                                                template={template.html}
+                                                socials={props.icons}
+                                            />
+                                        </li>
+                                    );
+                                }
+                            } else
+                                return (
+                                    <li key={template.id}>
+                                        <input
+                                            type="radio"
+                                            name="template"
+                                            value={template.html}
+                                        />
+                                        <Template
+                                            template={template.html}
+                                            socials={props.icons}
+                                            organisation={props.organisation}
+                                        />
+                                    </li>
+                                );
+                    })}
+                    {!tag ? (
+                        <li style={{ width: "412px", height: "220px" }}></li>
+                    ) : (
+                        ""
+                    )}
+                </ul>
+                <div className={classes.btnContainer}>
+                    <Button
+                        color="orange"
+                        width={"5rem"}
+                        onClick={(e) => {
+                            e.preventDefault();
+                            props.showFunction();
+                        }}
+                    >
+                        Annuler
+                    </Button>
+                </div>
+            </form>
+        </div>
+    );
 }
