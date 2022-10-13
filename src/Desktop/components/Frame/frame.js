591adb5972d4b47ddfeb65c00121e26fbb27bb84
var-poro <110412302+var-poro@users.noreply.github.com>
2022-10-13 15:19:35 +0200

    update logo & menu

--- src/Desktop/components/Frame/frame.js
+++ src/Desktop/components/Frame/frame.js
@@ -4,63 +4,91 @@ import { Suspense, useEffect, useState } from "react";
 import classes from "./frame.module.scss";
 import request from "Utils/Request/request";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function Frame(props) {
-    const [user, setUser] = useState()
-    const [organisation, setOrganisation] = useState()
+    const [user, setUser] = useState();
+    const [organisation, setOrganisation] = useState();
 
     useEffect(() => {
-        request.get('whoami').then((res) => {
-            setUser(res.data)
-            const sseUser = new EventSource(`${process.env.REACT_APP_HUB_URL}${res.data['@id']}`);
-            const sse = new EventSource(`${process.env.REACT_APP_HUB_URL}${res.data['organisation']}`);
+        request
+            .get("whoami")
+            .then((res) => {
+                setUser(res.data);
+                const sseUser = new EventSource(
+                    `${process.env.REACT_APP_HUB_URL}${res.data["@id"]}`
+                );
+                const sse = new EventSource(
+                    `${process.env.REACT_APP_HUB_URL}${res.data["organisation"]}`
+                );
 
-            function getRealtimeData(data) {
-                setOrganisation(data)
-            }
-            sse.onmessage = e => getRealtimeData(JSON.parse(e.data));
+                function getRealtimeData(data) {
+                    setOrganisation(data);
+                }
+                sse.onmessage = (e) => getRealtimeData(JSON.parse(e.data));
 
-            function getRealtimeDataUser(data) {
-                setUser(data)
-            }
-            sseUser.onmessage = e => getRealtimeDataUser(JSON.parse(e.data));
+                function getRealtimeDataUser(data) {
+                    setUser(data);
+                }
+                sseUser.onmessage = (e) =>
+                    getRealtimeDataUser(JSON.parse(e.data));
 
-            // return () => {
-            //     sse.close();
-            //     sseUser.close();
-            // };
-            request.get(res.data.organisation).then((r) => {
-                setOrganisation(r.data)
-            }).catch((err) => console.log(err))
-        }).catch((err) => { console.log(err) })
-    }, [])
+                // return () => {
+                //     sse.close();
+                //     sseUser.close();
+                // };
+                request
+                    .get(res.data.organisation)
+                    .then((r) => {
+                        setOrganisation(r.data);
+                    })
+                    .catch((err) => console.log(err));
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
 
-    return (<>
-        {user ?
-            <div className={classes.desktop}>
-                <div className={classes.desktopSubcontainer}>
-                    <Header page={props.path || ""} user={user} />
-                    <div className={classes.mainContent}>
-                        <div className={classes.menuContainer}>
-                            <Link to="/profile/informations/company">
+    return (
+        <>
+            {user ? (
+                <div className={classes.desktop}>
+                    <div className={classes.desktopSubcontainer}>
+                        <Header page={props.path || ""} user={user} />
+                        <div className={classes.mainContent}>
+                            <div className={classes.menuContainer}>
                                 <div className={classes.userInfos}>
-                                    <img src={organisation?.logo?.url ? (organisation?.logo?.url) : 'https://dummyimage.com/108/f4eeef.png'} alt='' />
-                                    <p className={classes.capitalize}>{organisation?.name}</p>
+                                    <img
+                                        src={
+                                            organisation?.logo?.url
+                                                ? organisation?.logo?.url
+                                                : "https://dummyimage.com/108/f4eeef.png"
+                                        }
+                                        alt=""
+                                    />
+                                    <p className={classes.capitalize}>
+                                        {organisation?.name}
+                                    </p>
                                 </div>
-                            </Link>
-                            <Menu className={classes.menu} page={props.path} />
-                        </div>
+                                <Menu
+                                    className={classes.menu}
+                                    page={props.path}
+                                />
+                            </div>
 
-                        <div className={classes.dashboardContainer}>
-                            <Suspense fallback={<div>Chargement</div>}>
-                                {props.children}
-                            </Suspense>
+                            <div className={classes.dashboardContainer}>
+                                <Suspense fallback={<div>Chargement</div>}>
+                                    {props.children}
+                                </Suspense>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            : <div className={classes.loading}><AiOutlineLoading3Quarters /></div>
-        }
-    </>)
+            ) : (
+                <div className={classes.loading}>
+                    <AiOutlineLoading3Quarters />
+                </div>
+            )}
+        </>
+    );
 }
