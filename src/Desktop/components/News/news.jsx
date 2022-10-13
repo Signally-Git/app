0e31df58c75a99607b30f5bdeae87e29182fc57a
var-poro <110412302+var-poro@users.noreply.github.com>
2022-10-13 10:24:26 +0200

    upgrade packages to fix HotReload issues update react/swiper remove unused tags in templateSelection

--- src/Desktop/components/News/news.jsx
+++ src/Desktop/components/News/news.jsx
@@ -1,104 +1,143 @@
 import React from "react";
 import classes from "./news.module.css";
 import OnBoarding from "./onBoarding/onBoarding";
-import { Pagination, A11y, Autoplay } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react/swiper-react.js';
+import { Pagination, A11y, Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 import parse from "html-react-parser";
 import request from "Utils/Request/request";
 import moment from "moment";
 
-import 'swiper/swiper.min.css';
+import "swiper/swiper.min.css";
 
 function News({ organisation, loading, setLoading }) {
-    const [org, setOrg] = React.useState(organisation)
-    const [users, setUsers] = React.useState(organisation.users)
-    const [events, setEvents] = React.useState(organisation.events)
-    const [slides, setSlides] = React.useState(['']);
-    const [swipe, setSwipe] = React.useState()
-    const [admin, setAdmin] = React.useState({ signature: { name: "" }, compiledSignature: "test" })
-    const [onboardingComplete, setOnboardingComplete] = React.useState(false)
+    const [org, setOrg] = React.useState(organisation);
+    const [users, setUsers] = React.useState(organisation.users);
+    const [events, setEvents] = React.useState(organisation.events);
+    const [slides, setSlides] = React.useState([""]);
+    const [swipe, setSwipe] = React.useState();
+    const [admin, setAdmin] = React.useState({
+        signature: { name: "" },
+        compiledSignature: "test",
+    });
+    const [onboardingComplete, setOnboardingComplete] = React.useState(false);
 
     const refreshOrganisation = async () => {
-        request.get('users').then((r) => {
-            setUsers(r.data['hydra:member'])
-        })
-        request.get('events').then((r) => {
-            setEvents(r.data['hydra:member'])
-        })
-        await request.get(organisation['@id']).then((res) => {
-            setOrg({ ...res.data, users: users, events: events })
-        })
-    }
+        request.get("users").then((r) => {
+            setUsers(r.data["hydra:member"]);
+        });
+        request.get("events").then((r) => {
+            setEvents(r.data["hydra:member"]);
+        });
+        await request.get(organisation["@id"]).then((res) => {
+            setOrg({ ...res.data, users: users, events: events });
+        });
+    };
 
     React.useEffect(() => {
-        setAdmin(localStorage.getItem('user'));
-        refreshOrganisation()
-        setLoading(loading + 1)
-    }, [organisation])
+        setAdmin(localStorage.getItem("user"));
+        refreshOrganisation();
+        setLoading(loading + 1);
+    }, [organisation]);
 
     React.useEffect(() => {
-        setSlides([])
-        const toPush = []
+        setSlides([]);
+        const toPush = [];
 
-        toPush.push(<><OnBoarding organisation={org} completed={onboardingComplete} setCompleted={setOnboardingComplete} /></>)
+        toPush.push(
+            <>
+                <OnBoarding
+                    organisation={org}
+                    completed={onboardingComplete}
+                    setCompleted={setOnboardingComplete}
+                />
+            </>
+        );
 
         if (admin?.compiledSignature)
-            toPush.push(<>
-                <h5>Signature active <span className={classes.orangeTxt}>{admin.signature.name}</span></h5>
-                <br />
-                {parse(admin.compiledSignature)}
-            </>)
+            toPush.push(
+                <>
+                    <h5>
+                        Signature active{" "}
+                        <span className={classes.orangeTxt}>
+                            {admin.signature.name}
+                        </span>
+                    </h5>
+                    <br />
+                    {parse(admin.compiledSignature)}
+                </>
+            );
 
         if (admin.events?.[0]?.name)
-            toPush.push(<>
-                <h5>
-                    Event actif{" "}
-                    <span className={classes.orangeTxt}>
-                        {admin.events[0]?.name}
-                    </span>
-                </h5>
-                <div className={classes.preview}>
-                    <img src={process.env.REACT_APP_API_URL + '/' + admin.events[0]?.imagePath} />
-                    <span className={classes.duration}>
-                        <div className={`${classes.col} ${classes.bold}`}>
-                            <span>{`du ${moment.utc(admin.events[0]?.startAt).local(false).format('D MMM YYYY')}`}</span>
-                            <span>{`au ${moment.utc(admin.events[0]?.endAt).local(false).format('D MMM YYYY')}`}</span>
-                        </div>
-                        <div className={classes.col}>
-                            <span>{`${moment.utc(admin.events[0]?.startAt).local(false).format('HH:mm')}`}</span>
-                            <span>{`${moment.utc(admin.events[0]?.endAt).local(false).format('HH:mm')}`}</span>
-                        </div>
-                    </span>
-                </div>
-            </>)
-        setSlides(toPush)
-    }, [onboardingComplete, organisation, admin, org])
+            toPush.push(
+                <>
+                    <h5>
+                        Event actif{" "}
+                        <span className={classes.orangeTxt}>
+                            {admin.events[0]?.name}
+                        </span>
+                    </h5>
+                    <div className={classes.preview}>
+                        <img
+                            src={
+                                process.env.REACT_APP_API_URL +
+                                "/" +
+                                admin.events[0]?.imagePath
+                            }
+                        />
+                        <span className={classes.duration}>
+                            <div className={`${classes.col} ${classes.bold}`}>
+                                <span>{`du ${moment
+                                    .utc(admin.events[0]?.startAt)
+                                    .local(false)
+                                    .format("D MMM YYYY")}`}</span>
+                                <span>{`au ${moment
+                                    .utc(admin.events[0]?.endAt)
+                                    .local(false)
+                                    .format("D MMM YYYY")}`}</span>
+                            </div>
+                            <div className={classes.col}>
+                                <span>{`${moment
+                                    .utc(admin.events[0]?.startAt)
+                                    .local(false)
+                                    .format("HH:mm")}`}</span>
+                                <span>{`${moment
+                                    .utc(admin.events[0]?.endAt)
+                                    .local(false)
+                                    .format("HH:mm")}`}</span>
+                            </div>
+                        </span>
+                    </div>
+                </>
+            );
+        setSlides(toPush);
+    }, [onboardingComplete, organisation, admin, org]);
 
     React.useEffect(() => {
         setTimeout(() => {
-            swipe?.updateAutoHeight()
+            swipe?.updateAutoHeight();
         }, 10);
-    }, [onboardingComplete])
+    }, [onboardingComplete]);
 
-    return (<div className={classes.container}>
-        <Swiper modules={[Pagination, A11y, Autoplay]}
-            spaceBetween={50}
-            className={classes.swiper}
-            allowTouchMove={false}
-            autoHeight={true}
-            onInit={(e) => { refreshOrganisation(); setSwipe(e) }}
-            pagination={{ type: 'bullets', clickable: true }}
-            onSlideChange={(e) => console.log(e)}
-        >
-
-            {slides?.map((slide, index) => {
-                return (<SwiperSlide key={index}>
-                    {slide}
-                </SwiperSlide>)
-            })}
-        </Swiper>
-
-    </div>
+    return (
+        <div className={classes.container}>
+            <Swiper
+                modules={[Pagination, A11y, Autoplay]}
+                spaceBetween={50}
+                className={classes.swiper}
+                allowTouchMove={false}
+                autoHeight={true}
+                onInit={(e) => {
+                    refreshOrganisation();
+                    setSwipe(e);
+                }}
+                pagination={{ type: "bullets", clickable: true }}
+                onSlideChange={(e) => console.log(e)}
+            >
+                {slides?.map((slide, index) => {
+                    return <SwiperSlide key={index}>{slide}</SwiperSlide>;
+                })}
+            </Swiper>
+        </div>
     );
 }
 
--- src/Desktop/views/Signatures/create/TemplateSelect/templateSelect.js
+++ src/Desktop/views/Signatures/create/TemplateSelect/templateSelect.js
@@ -3,149 +3,151 @@ import { useState, useEffect } from "react";
 import Button from "Utils/Button/btn";
 import Template from "../Preview/customizablePreview";
 import request from "Utils/Request/request";
-import {AiOutlineLoading3Quarters} from "react-icons/ai";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 // Displaying the list of bought and free templates (Studio, Store) and allows to select one to create custom signature
 
 export default function TemplateSelection(props) {
-	const [orientation, setOrientation] = useState("Horizontal");
+    const [orientation, setOrientation] = useState("Horizontal");
     const [fetching, setFetching] = useState(true);
-	const [tag, setTag] = useState(true);
-	const [templatesList, setTemplatesList] = useState([])
+    const [tag, setTag] = useState(true);
+    const [templatesList, setTemplatesList] = useState([]);
 
-	const handleForm = (e) => {
-		props.setTemplate(JSON.parse(e.target.value));
-	};
+    const handleForm = (e) => {
+        props.setTemplate(JSON.parse(e.target.value));
+    };
 
-	const handleAlignment = (e) => {
-		setOrientation(e.target.id);
-	};
-	const handleStudio = (e) => {
-		setTag(e.target.checked);
-	};
+    const handleAlignment = (e) => {
+        setOrientation(e.target.id);
+    };
+    const handleStudio = (e) => {
+        setTag(e.target.checked);
+    };
 
-	useEffect(() => {
-		request.get('signature_templates').then((res) => {
-			setTemplatesList(res.data['hydra:member'])
-            setFetching(false)
-		})
-	}, [])
+    useEffect(() => {
+        request.get("signature_templates").then((res) => {
+            setTemplatesList(res.data["hydra:member"]);
+            setFetching(false);
+        });
+    }, []);
 
-    if(fetching)
-        return <div className={classes.modal}>
-            <AiOutlineLoading3Quarters className={classes.loading} />
+    if (fetching)
+        return (
+            <div className={classes.modal}>
+                <AiOutlineLoading3Quarters className={classes.loading} />
+            </div>
+        );
+    return (
+        <div className={classes.modal}>
+            <div className={classes.searchContainer}>
+                <h1>Choisissez votre modèle de signature</h1>
+                {/*<div className={classes.tagsContainer}>*/}
+                {/*	<form onChange={handleStudio}>*/}
+                {/*		<ul className={classes.studioStore}>*/}
+                {/*			<li*/}
+                {/*				className={`${classes.studio} ${tag ? classes.activeStudio : ""*/}
+                {/*					}`}*/}
+                {/*			>*/}
+                {/*				<input*/}
+                {/*					type="checkbox"*/}
+                {/*					defaultChecked={tag}*/}
+                {/*					id="studio"*/}
+                {/*				/>*/}
+                {/*				My Studio*/}
+                {/*			</li>*/}
+                {/*			<li className={classes.store}>My Store</li>*/}
+                {/*		</ul>*/}
+                {/*	</form>*/}
+                {/*	<div className={classes.otherTagsContainer}>*/}
+                {/*		<ul className={classes.otherTags}>*/}
+                {/*			<li>Classique</li>*/}
+                {/*			<li>Élegant</li>*/}
+                {/*			<li>Créatif</li>*/}
+                {/*		</ul>*/}
+                {/*	</div>*/}
+                {/*</div>*/}
+                {/*<div className={classes.orientationContainer}>*/}
+                {/*	<form onChange={handleAlignment}>*/}
+                {/*		<label*/}
+                {/*			className={classes.radioCtr}*/}
+                {/*			htmlFor="horizontal"*/}
+                {/*		>*/}
+                {/*			Horizontal*/}
+                {/*			<input*/}
+                {/*				type="radio"*/}
+                {/*				defaultChecked={true}*/}
+                {/*				id="horizontal"*/}
+                {/*				name="orientation"*/}
+                {/*			/>*/}
+                {/*			<span className={classes.checkmark}></span>*/}
+                {/*		</label>*/}
+                {/*		<label*/}
+                {/*			className={classes.radioCtr}*/}
+                {/*			htmlFor="panoramique"*/}
+                {/*		>*/}
+                {/*			Panoramique*/}
+                {/*			<input*/}
+                {/*				type="radio"*/}
+                {/*				id="panoramique"*/}
+                {/*				name="orientation"*/}
+                {/*			/>*/}
+                {/*			<span className={classes.checkmark}></span>*/}
+                {/*		</label>*/}
+                {/*		<label className={classes.radioCtr} htmlFor="vertical">*/}
+                {/*			Vertical*/}
+                {/*			<input*/}
+                {/*				type="radio"*/}
+                {/*				id="vertical"*/}
+                {/*				name="orientation"*/}
+                {/*			/>*/}
+                {/*			<span className={classes.checkmark}></span>*/}
+                {/*		</label>*/}
+                {/*	</form>*/}
+                {/*</div>*/}
+            </div>
+            <form onChange={handleForm}>
+                <ul className={classes.templatesContainer}>
+                    {templatesList?.map((template) => {
+                        return (
+                            <li key={template.id}>
+                                <p className={classes.templateName}>
+                                    {template.name}
+                                </p>
+                                <input
+                                    readOnly
+                                    type="radio"
+                                    name="template"
+                                    value={JSON.stringify(template)}
+                                />
+                                <Template
+                                    options={{
+                                        event: { enabled: true },
+                                    }}
+                                    template={template.html}
+                                    socials={props.icons}
+                                />
+                            </li>
+                        );
+                    })}
+                    {!tag ? (
+                        <li style={{ width: "412px", height: "220px" }}></li>
+                    ) : (
+                        ""
+                    )}
+                </ul>
+                <div className={classes.btnContainer}>
+                    <Button
+                        color="orange"
+                        width={"5rem"}
+                        onClick={(e) => {
+                            e.preventDefault();
+                            props.showFunction(false, "smooth");
+                        }}
+                    >
+                        Annuler
+                    </Button>
+                </div>
+            </form>
         </div>
-	return (
-		<div className={classes.modal}>
-			<div className={classes.searchContainer}>
-				<h1>Choisissez votre modèle de signature</h1>
-				<div className={classes.tagsContainer}>
-					<form onChange={handleStudio}>
-						<ul className={classes.studioStore}>
-							<li
-								className={`${classes.studio} ${tag ? classes.activeStudio : ""
-									}`}
-							>
-								<input
-									type="checkbox"
-									defaultChecked={tag}
-									id="studio"
-								/>
-								My Studio
-							</li>
-							<li className={classes.store}>My Store</li>
-						</ul>
-					</form>
-					<div className={classes.otherTagsContainer}>
-						<ul className={classes.otherTags}>
-							<li>Classique</li>
-							<li>Élegant</li>
-							<li>Créatif</li>
-						</ul>
-					</div>
-				</div>
-				<div className={classes.orientationContainer}>
-					<form onChange={handleAlignment}>
-						<label
-							className={classes.radioCtr}
-							htmlFor="horizontal"
-						>
-							Horizontal
-							<input
-								type="radio"
-								defaultChecked={true}
-								id="horizontal"
-								name="orientation"
-							/>
-							<span className={classes.checkmark}></span>
-						</label>
-						<label
-							className={classes.radioCtr}
-							htmlFor="panoramique"
-						>
-							Panoramique
-							<input
-								type="radio"
-								id="panoramique"
-								name="orientation"
-							/>
-							<span className={classes.checkmark}></span>
-						</label>
-						<label className={classes.radioCtr} htmlFor="vertical">
-							Vertical
-							<input
-								type="radio"
-								id="vertical"
-								name="orientation"
-							/>
-							<span className={classes.checkmark}></span>
-						</label>
-					</form>
-				</div>
-			</div>
-			<form onChange={handleForm}>
-				<ul className={classes.templatesContainer}>
-					{templatesList?.map((template) => {
-									return (
-										<li key={template.id}>
-											<p className={classes.templateName}>
-												{template.name}
-											</p>
-											<input
-												readOnly
-												type="radio"
-												name="template"
-												value={JSON.stringify(template)}
-											/>
-											<Template
-												options={{
-													event: { enabled: true },
-												}}
-												template={template.html}
-												socials={props.icons}
-											/>
-										</li>
-									);
-					})}
-					{!tag ? (
-						<li style={{ width: "412px", height: "220px" }}></li>
-					) : (
-						""
-					)}
-				</ul>
-				<div className={classes.btnContainer}>
-					<Button
-						color="orange"
-						width={"5rem"}
-						onClick={(e) => {
-							e.preventDefault();
-							props.showFunction(false, 'smooth');
-						}}
-					>
-						Annuler
-					</Button>
-				</div>
-			</form>
-		</div>
-	);
+    );
 }
