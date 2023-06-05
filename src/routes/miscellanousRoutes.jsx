f53acfd1dc35f14ddd494df60ee4cd3c4b610d2a
var-poro <110412302+var-poro@users.noreply.github.com>
2023-06-05 14:12:14 +0200

    🔥Remove unused route Feedback

--- src/routes/miscellanousRoutes.jsx
+++ src/routes/miscellanousRoutes.jsx
@@ -1,9 +1,6 @@
 import PrivateRoute from "../Utils/PrivateRoute/privateRoute";
 import ImgUploader from "../views/imgUploader/imgUploader";
-import Report from "../containers/Feedback";
 
 export const miscellanousRoutes = [
     <PrivateRoute path="/upload-img" component={ImgUploader} />,
-
-    <PrivateRoute exact path="/report" component={Report} />,
 ];
