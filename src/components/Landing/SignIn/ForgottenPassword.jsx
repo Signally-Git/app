42b57de9be55ba01747d59639a652ed4d7b859b6
var-poro <110412302+var-poro@users.noreply.github.com>
2023-06-05 13:52:06 +0200

    👔Adds legals & tos

---
+++ src/components/Landing/SignIn/ForgottenPassword.jsx
@@ -0,0 +1,77 @@
+import axios from "axios";
+import classes from "../landing.module.css";
+import { FormattedMessage, useIntl } from "react-intl";
+import Button from "Utils/Button/btn";
+import Input from "Utils/Input/input";
+import React, { useRef, useState } from "react";
+
+export const ForgottenPassword = ({ isOpen, setIsOpen }) => {
+    const [email, setEmail] = useState("");
+
+    const toFocusMail = useRef(null);
+
+    const intl = useIntl();
+    const handleForgotSubmit = (e) => {
+        e.preventDefault();
+        axios
+            .post(`${process.env.REACT_APP_API_URL}/reset_password`, {
+                email: email,
+            })
+            .then(() => {
+                setIsOpen("done");
+            });
+    };
+
+    if (isOpen === "done") {
+        return (
+            <div className={classes.modal}>
+                <p>
+                    <FormattedMessage id="message.success.sign_up.part1" />
+                    <span className={classes.orangeTxt}> {email} </span>
+                    <FormattedMessage id="message.success.sign_up.part2" />
+                </p>
+                <Button
+                    color={"orange"}
+                    width={"40%"}
+                    onClick={() => setIsOpen(null)}
+                >
+                    <FormattedMessage id="buttons.placeholder.confirm" />
+                </Button>
+            </div>
+        );
+    } else if (isOpen !== null) {
+        return (
+            <form
+                onSubmit={(e) => handleForgotSubmit(e)}
+                className={classes.modal}
+            >
+                <h3>
+                    <FormattedMessage id="reset_password" />
+                </h3>
+                <Input
+                    ref={toFocusMail}
+                    defaultValue={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    type="mail"
+                    style={{ width: "100%" }}
+                    autoComplete="email"
+                    placeholder={intl.formatMessage({ id: "email" })}
+                />
+                <div className={classes.btnsContainer}>
+                    <Button type="submit" color={"orangeFill"} width={"40%"}>
+                        <FormattedMessage id="buttons.placeholder.send" />
+                    </Button>
+                    <Button
+                        type="button"
+                        color={"orange"}
+                        width={"40%"}
+                        onClick={() => setIsOpen(null)}
+                    >
+                        <FormattedMessage id="buttons.placeholder.cancel" />
+                    </Button>
+                </div>
+            </form>
+        );
+    }
+    return <></>;
+};
