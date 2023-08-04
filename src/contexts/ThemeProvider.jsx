ba36b1d88bbd8d613a105554deda8476391b7b4f
var-poro <110412302+var-poro@users.noreply.github.com>
2023-08-04 16:38:02 +0200

    🚧WIP: window location origin instead of href

--- src/contexts/ThemeProvider.jsx
+++ src/contexts/ThemeProvider.jsx
@@ -5,7 +5,7 @@ import { AiOutlineLoading3Quarters } from "react-icons/ai";
 const ThemeContext = createContext();
 const ThemeProvider = ({ children }) => {
     const [theme, setTheme] = useState(null);
-    const url = window.location.href;
+    const url = window.location.origin;
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
