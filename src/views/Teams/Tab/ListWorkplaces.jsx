ef71c88b5a6d913b2bdb7a51fd636373f9a7f703
var-poro <110412302+var-poro@users.noreply.github.com>
2023-11-02 18:25:08 +0100

    ✨ lock workplace hover when editing

--- src/views/Teams/Tab/ListWorkplaces.jsx
+++ src/views/Teams/Tab/ListWorkplaces.jsx
@@ -68,6 +68,10 @@ export const ListWorkplaces = ({
         setIsDeployed(editInfo?.synchronizable || false);
     }, [editInfo]);
 
+    useEffect(() => {
+        console.log(edit);
+    }, [edit]);
+
     const handleChangeWP = async (e, workplace) => {
         const img = new FormData();
         img.append("file", file);
@@ -183,8 +187,9 @@ export const ListWorkplaces = ({
                             return (
                                 <li
                                     onMouseMove={() => {
-                                        if (edit === "" || !editInfo) {
+                                        if (!edit) {
                                             clearTimeout(time);
+                                            console.log(edit, editInfo);
                                             time = setTimeout(() => {
                                                 setSelected(workplace);
                                             }, 100);
